5ec7857bda917c6ff154a6f0c8ecc3a9e1d4fd3f
mrprabbs <pkukreja2002@gmail.com>
2022-11-30 12:21:51 -0600

    Delete config.py

--- config.py
+++
@@ -1,7 +0,0 @@
-# Google Calendar API Config
-
-calendarID = 'c_740q6enc6gpo32uirphosr4dfc@group.calendar.google.com'
-
-# REDCap API Config
-api_url = 'https://redcap-tst.ictr.wisc.edu/api/'
-api_key = '2C5C106279D8A59867CEF329EDBB2C7A'
